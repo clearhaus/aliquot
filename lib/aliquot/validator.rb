f445fcdc4cdf359b9444758a88f8757ddef714e4
Christian Bobach <cb@clearhaus.com>
2022-08-31 10:32:41 +0200

    `ruby` update to `2.7.4`.
    dry-validation -> 1.8
    ruby -> 2.7.4

--- lib/aliquot/validator.rb
+++ lib/aliquot/validator.rb
@@ -9,143 +9,240 @@ module Aliquot
   module Validator
     # Verified according to:
     # https://developers.google.com/pay/api/web/guides/resources/payment-data-cryptography#payment-method-token-structure
-    module Predicates
-      include Dry::Logic::Predicates
 
-      CUSTOM_PREDICATE_ERRORS = {
-        base64?:          'must be Base64',
-        pan?:             'must be a pan',
-        ec_public_key?:   'must be an EC public key',
-        eci?:             'must be an ECI',
-        json_string?:     'must be valid JSON',
-        integer_string?:  'must be string encoded integer',
-        month?:           'must be a month (1..12)',
-        year?:            'must be a year (2000..3000)',
-        base64_asn1?:     'must be base64 encoded asn1 value',
-        json_object?:     'must be a JSON object',
+    CUSTOM_PREDICATE_ERRORS = {
+      is_base64: 'must be Base64',
+      is_pan: 'must be a pan',
+      is_ec_public_key: 'must be an EC public key',
+      is_eci: 'must be an ECI',
+      is_integer_string: 'must be string encoded integer',
+      is_month: 'must be a month (1..12)',
+      is_year: 'must be a year (2000..3000)',
+      is_base64_asn1: 'must be base64 encoded asn1 value',
+      is_json: 'must be valid JSON',
 
-        authMethodCryptogram3DS: 'authMethod CRYPTOGRAM_3DS requires eciIndicator',
-        authMethodCard:          'eciIndicator/cryptogram must be omitted when PAN_ONLY',
-      }.freeze
+      is_authMethodCryptogram3DS: 'authMethod CRYPTOGRAM_3DS requires eciIndicator',
+      is_authMethodCard: 'eciIndicator/cryptogram must be omitted when PAN_ONLY',
+    }.freeze
 
-      # Support Ruby 2.3, but use the faster #match? when available.
-      match_b = ''.respond_to?(:match?) ? ->(s, re) { s.match?(re) } : ->(s, re) { !!(s =~ re) }
-
-      def self.to_bool(lbd)
-        lbd.call
-        true
-      rescue
-        false
-      end
-
-      predicate(:base64?) do |x|
-        str?(x) &&
-          match_b.call(x, /\A[=A-Za-z0-9+\/]*\z/) && # allowable chars
-          x.length.remainder(4).zero? && # multiple of 4
-          !match_b.call(x, /=[^$=]/) && # may only end with ='s
-          !match_b.call(x, /===/) # at most 2 ='s
-      end
-
-      # We should figure out how strict we should be. Hopefully we can discard
-      # the above Base64? predicate and use the following simpler one:
-      #predicate(:strict_base64?) { |x| !!Base64.strict_decode64(x) rescue false }
-
-      predicate(:pan?) { |x| match_b.call(x, /\A[1-9][0-9]{11,18}\z/) }
-
-      predicate(:eci?) { |x| str?(x) && match_b.call(x, /\A\d{1,2}\z/) }
-
-      predicate(:ec_public_key?) { |x| base64?(x) && OpenSSL::PKey::EC.new(Base64.decode64(x)).check_key rescue false }
-
-      predicate(:json_string?) { |x| !!JSON.parse(x) rescue false }
-
-      predicate(:integer_string?) { |x| str?(x) && match_b.call(x, /\A\d+\z/) }
-
-      predicate(:month?) { |x| x.between?(1, 12) }
-
-      predicate(:year?) { |x| x.between?(2000, 3000) }
-
-      predicate(:base64_asn1?) { |x| OpenSSL::ASN1.decode(Base64.strict_decode64(x)) rescue false }
-
-      predicate(:json_object?) { |x| hash?(x) }
+    def self.base64_check(value)
+      /\A[=A-Za-z0-9+\/]*\z/.match?(value) &&
+        value.length.remainder(4).zero? &&
+        !/=[^$=]/.match?(value) &&
+        !/===/.match?(value)
     end
 
-    # Base for DRY-Validation schemas used in Aliquot.
-    class BaseSchema < Dry::Validation::Schema::JSON
-      predicates(Predicates)
-      def self.messages
-        super.merge(en: { errors: Predicates::CUSTOM_PREDICATE_ERRORS })
+    Dry::Validation.register_macro(:is_base64) do
+      if key?
+        unless Aliquot::Validator.base64_check(value)
+          key.failure(CUSTOM_PREDICATE_ERRORS[:is_base64])
+        end
       end
     end
+
+    def self.ans1_check(value)
+      OpenSSL::ASN1.decode(Base64.strict_decode64(value)) rescue false
+    end
+
+    Dry::Validation.register_macro(:is_base64_asn1) do
+      if key?
+        unless Aliquot::Validator.ans1_check(value)
+          key.failure(CUSTOM_PREDICATE_ERRORS[:is_base64_asn1])
+        end
+      end
+    end
+
+    Dry::Validation.register_macro(:is_pan) do
+      if key?
+        unless /\A[1-9][0-9]{11,18}\z/.match?(value)
+          key.failure(CUSTOM_PREDICATE_ERRORS[:is_pan])
+        end
+      end
+    end
+
+    Dry::Validation.register_macro(:is_ec_public_key) do
+      if key?
+        ec = -> () { OpenSSL::PKey::EC.new(Base64.decode64(value)).check_key rescue false }.call
+        unless :is_base64 && ec
+          key.failure(CUSTOM_PREDICATE_ERRORS[:is_ec_public_key])
+        end
+      end
+    end
+
+    Dry::Validation.register_macro(:is_eci) do
+      if key?
+        unless /\A\d{1,2}\z/.match?(value)
+          key.failure(CUSTOM_PREDICATE_ERRORS[:is_eci])
+        end
+      end
+    end
+
+    Dry::Validation.register_macro(:is_integer_string) do
+      if key?
+        unless /\A\d+\z/.match?(value)
+          key.failure(CUSTOM_PREDICATE_ERRORS[:is_integer_string])
+        end
+      end
+    end
+
+    Dry::Validation.register_macro(:is_json) do
+      if key?
+        is_json = -> () { !!JSON.parse(value) rescue false }.call
+        unless is_json
+          key.failure(CUSTOM_PREDICATE_ERRORS[:is_json])
+        end
+      end
+    end
+
+    Dry::Validation.register_macro(:is_month) do
+      if key?
+        unless value.between?(1, 12)
+          key.failure(CUSTOM_PREDICATE_ERRORS[:is_month])
+        end
+      end
+    end
+
+    Dry::Validation.register_macro(:is_year) do
+      if key?
+        unless value.between?(2000, 3000)
+          key.failure(CUSTOM_PREDICATE_ERRORS[:is_year])
+        end
+      end
+    end
+
+    class SignedKeyContract < Dry::Validation::Contract
+      json do
+        required(:keyExpiration).filled(:str?)
+        required(:keyValue).filled(:str?)
+      end
+      rule(:keyExpiration).validate(:is_integer_string)
+      rule(:keyValue).validate(:is_ec_public_key)
+    end
+    SignedKeySchema = SignedKeyContract.new
 
     # Schema used for the 'intermediateSigningKey' hash included in ECv2.
-    IntermediateSigningKeySchema = Dry::Validation.Schema(BaseSchema) do
-      required(:signedKey).filled(:str?, :json_string?)
-
-      required(:signatures).filled(:array?) { each { base64? & base64_asn1? } }
+    class IntermediateSigningKeyContract < Dry::Validation::Contract
+      json do
+        required(:signedKey).filled(:str?)
+        required(:signatures).array(:str?)
+      end
+      rule(:signedKey).validate(:is_json)
+      rule(:signatures).each do
+        key.failure('must be Base64') unless Aliquot::Validator.base64_check(value) &&
+          Aliquot::Validator.ans1_check(value)
+      end
     end
-
-    SignedKeySchema = Dry::Validation.Schema(BaseSchema) do
-      required(:keyExpiration).filled(:integer_string?)
-      required(:keyValue).filled(:ec_public_key?)
-    end
+    IntermediateSigningKeySchema = IntermediateSigningKeyContract.new
 
     # DRY-Validation schema for Google Pay token
-    TokenSchema = Dry::Validation.Schema(BaseSchema) do
-      required(:signature).filled(:str?, :base64?, :base64_asn1?)
-
-      required(:protocolVersion).filled(:str?).when(eql?: 'ECv2') do
-        required(:intermediateSigningKey)
+    class TokenContract < Dry::Validation::Contract
+      json do
+        required(:signature).filled(:str?)
+        required(:signedMessage).filled(:str?)
+        required(:protocolVersion).filled(:str?)
+        optional(:intermediateSigningKey).filled(:hash?).schema(IntermediateSigningKeyContract.new.schema)
       end
+      rule(:signature).validate(:is_base64, :is_base64_asn1)
+      rule(:signedMessage).validate(:is_json)
 
-      required(:signedMessage).filled(:str?, :json_string?)
-
-      optional(:intermediateSigningKey).value(:json_object?) { schema(IntermediateSigningKeySchema) }
+      # Old rule:
+      # required(:protocolVersion).filled(:str?).when(eql?: 'ECv2') do
+      #   required(:intermediateSigningKey)
+      # end
+      #
+      # if :protocolVersion is 'ECv2' => require :intermediateSigningKey
+      rule(:intermediateSigningKey) do
+        key.failure('is missing') if 'ECv2'.eql?(values[:protocolVersion]) &&
+          values[:intermediateSigningKey].nil?
+      end
     end
+    TokenSchema = TokenContract.new
 
     # DRY-Validation schema for signedMessage component Google Pay token
-    SignedMessageSchema = Dry::Validation.Schema(BaseSchema) do
-      required(:encryptedMessage).filled(:str?, :base64?)
-      required(:ephemeralPublicKey).filled(:str?, :base64?)
-      required(:tag).filled(:str?, :base64?)
+    class SignedMessageContract < Dry::Validation::Contract
+      json do
+        required(:encryptedMessage).filled(:str?)
+        required(:ephemeralPublicKey).filled(:str?)
+        required(:tag).filled(:str?)
+      end
+      rule(:encryptedMessage).validate(:is_base64)
+      rule(:ephemeralPublicKey).validate(:is_base64)
+      rule(:tag).validate(:is_base64)
     end
+    SignedMessageSchema = SignedMessageContract.new
 
     # DRY-Validation schema for paymentMethodDetails component Google Pay token
-    PaymentMethodDetailsSchema = Dry::Validation.Schema(BaseSchema) do
-      required(:pan).filled(:integer_string?, :pan?)
-      required(:expirationMonth).filled(:int?, :month?)
-      required(:expirationYear).filled(:int?, :year?)
-      required(:authMethod).filled(:str?, included_in?: %w[PAN_ONLY CRYPTOGRAM_3DS])
+    class PaymentMethodDetailsContract < Dry::Validation::Contract
+      json do
+        required(:pan).filled(:str?)
+        required(:expirationMonth).filled(:int?)
+        required(:expirationYear).filled(:int?)
+        required(:authMethod).filled(:str?, included_in?: %w[PAN_ONLY CRYPTOGRAM_3DS])
 
-      optional(:cryptogram).filled(:str?)
-      optional(:eciIndicator).filled(:str?, :eci?)
+        optional(:cryptogram).filled(:str?)
+        optional(:eciIndicator).filled(:str?)
+      end
+      rule(:pan).validate(:is_integer_string, :is_pan)
+      rule(:expirationMonth).validate(:is_month)
+      rule(:expirationYear).validate(:is_year)
+      rule(:eciIndicator).validate(:is_eci)
 
-      rule(cryptogram: %i[authMethod cryptogram]) do |method, cryptogram|
-        method.eql?('CRYPTOGRAM_3DS') > required(:cryptogram)
+      # Old rule:
+      # rule(cryptogram: %i[authMethod cryptogram]) do |method, cryptogram|
+      #   method.eql?('CRYPTOGRAM_3DS') > required(:cryptogram)
+      # end
+      #
+      # if :authMethod is 'CRYPTOGRAM_3DS' => require :cryptogram
+      rule(:cryptogram) do
+        key.failure('is missing') if 'CRYPTOGRAM_3DS'.eql?(values[:authMethod]) &&
+          values[:cryptogram].nil?
       end
 
-      rule(eciIndicator: %i[authMethod eciIndicator]) do |method, eci|
-        method.eql?('PAN_ONLY').then(eci.none?)
+      # Old rule:
+      # rule(cryptogram: %i[authMethod cryptogram]) do |method, cryptogram|
+      #   method.eql?('PAN_ONLY').then(cryptogram.none?)
+      # end
+      #
+      # if :authMethod is 'PAN_ONLY' => no :cryptogram
+      rule(:cryptogram) do
+        key.failure('cannot be defined') if 'PAN_ONLY'.eql?(values[:authMethod]) &&
+          !values[:cryptogram].nil?
       end
 
-      rule(cryptogram: %i[authMethod cryptogram]) do |method, cryptogram|
-        method.eql?('PAN_ONLY').then(cryptogram.none?)
+      # Old rule:
+      # rule(eciIndicator: %i[authMethod eciIndicator]) do |method, eci|
+      #   method.eql?('PAN_ONLY').then(eci.none?)
+      # end
+      #
+      # if :authMethod is 'PAN_ONLY' => no :eciIndicator
+      rule(:eciIndicator) do
+        key.failure('cannot be defined') if 'PAN_ONLY'.eql?(values[:authMethod]) &&
+          !values[:eciIndicator].nil?
       end
     end
+    PaymentMethodDetailsSchema = PaymentMethodDetailsContract.new
 
     # DRY-Validation schema for encryptedMessage component Google Pay token
-    EncryptedMessageSchema = Dry::Validation.Schema(BaseSchema) do
-      required(:messageExpiration).filled(:str?, :integer_string?)
-      required(:messageId).filled(:str?)
-      required(:paymentMethod).filled(:str?, eql?: 'CARD')
-      required(:paymentMethodDetails).value(:json_object?) { schema PaymentMethodDetailsSchema }
+    class EncryptedMessageContract < Dry::Validation::Contract
+      json do
+        required(:messageExpiration).filled(:str?)
+        required(:messageId).filled(:str?)
+        required(:paymentMethod).filled(:str?)
+        required(:paymentMethodDetails).filled(:hash).schema(PaymentMethodDetailsContract.schema)
+      end
+      rule(:messageExpiration).validate(:is_integer_string)
+      rule(:paymentMethod) do
+        key.failure('must be equal to CARD') unless 'CARD'.eql?(value)
+      end
     end
+    EncryptedMessageSchema = EncryptedMessageContract.new
 
     module InstanceMethods
       attr_reader :output
 
       def validate
         @validation ||= @schema.call(@input)
-        @output = @validation.output
+        @output = @validation.values.data
         return true if @validation.success?
         raise Aliquot::ValidationError, "validation error(s), #{errors_formatted}"
       end
---
+++ spec/expectations/schema.rb
@@ -0,0 +1,50 @@
+require 'rspec/expectations'
+
+RSpec::Matchers.define :satisfy_schema do |expected|
+  match do |actual|
+    @check = expected.call(actual)
+    @check.success?
+  end
+
+  failure_message do
+    <<~EOM
+      expected that the given hash satisfy the schema, but:
+        #{@check.pretty_inspect}
+    EOM
+  end
+end
+
+RSpec::Matchers.define :dissatisfy_schema do |expected, mismatch|
+  match do |actual|
+    check = expected.call(actual)
+    @mismatches = mismatch
+    @errors = actual.messages
+
+    return false if check.success?
+
+    return true unless mismatch
+
+    key = mismatch.keys[0]
+    filtered_messages = actual.messages.select { |message| message.path[0] == key }
+
+    return false unless filtered_messages.length > 0
+
+    value = mismatch[key][0]
+    filtered_texts = (value.is_a? Array) ?
+                       filtered_messages.select { |message| message.text == value[0] } :
+                       filtered_messages.select { |message| message.text == value }
+
+    return false unless filtered_texts.length == 1
+
+    true
+  end
+
+  failure_message do
+    <<~EOM
+      expected that the given hash unsatisfy the schema this way:
+        #{@mismatches.pretty_inspect}
+      but got:
+        #{@errors.pretty_inspect}
+    EOM
+  end
+end
